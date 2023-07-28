fa3f533b5c614a470b1cb173b31e3c7a7eaeacb6
amitbadala <amitbadala07@gmail.com>
2023-07-28 17:17:13 +0530

    About update

--- src/views/About.js
+++ src/views/About.js
@@ -1,27 +1,81 @@
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "daily-activity-graph";
+import { Col, Row } from "reactstrap";
+import { Icon } from "@iconify/react";
+
+function ActivityChart({ data }) {
+  const chartRef = useRef();
+
+  return <div />;
+}
+
 const About = () => {
+  const data = Array.from({ length: 365 }).map((_, i) => {
+    return {
+      date: new Date(2023, 0, i + 1), // Generate dates for every day of the year
+      count: Math.floor(Math.random() * 60), // Random count for each day
+    };
+  });
+
+  const [chartData, setChartData] = useState(data);
+
+  // useEffect(() => {
+  //   const chart = new Chart("#chart", chartData);
+  //   chart.initialize();
+  //   chart.render();
+  // }, [chartData]);
+
   return (
     <section id="about" className="section">
+      {/* <ActivityChart data={data} /> */}
+      <div id="chart"></div>
       <div className="container">
-        <div>
+        <div className="about-content">
           <p className="typing-text">Hi, my name is</p>
           <h1 className="big-header dark-slate">
-            Amit Badala <span className="waving-hand">👋</span>
+            Amit Badala <span className="waving-hand">👋🏼</span>
           </h1>
           <h1 className="sub-big-header">
-            I am <span className="underline-magical">Software Engineer</span>{" "}
+            I am a <span className="underline-magical">Software Engineer</span>{" "}
             <br /> with 7+ yrs of exp.
           </h1>
-          <p>
-            I love javascript and everything on web, when my dev-senses kick in
-            I build presumably awesome stuff. I've always been drawn to the
-            overlap between{" "}
-            <span className="underline-magical">design and development</span>.
+          <p style={{ fontSize: 20 }}>
+            I like to craft solid and scalable frontend products with great user
+            experiences.
+            {/* <span className="underline-magical">design and development</span>. */}
           </p>
-          <p>
-            I have built a diverse range of applications across different
-            industries and have led the architecture of several Front End & Back
-            End projects from scratch.
-          </p>
+          <Row>
+            <Col md="3">
+              {/* <img
+                alt="..."
+                className="avatar border-gray"
+                src={require("assets/img/da-vinci.gif")}
+              /> */}
+              {/* <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              ></div> */}
+              {/* <Icon
+                icon="file-icons:ionic-project"
+                color="#333"
+                fontSize={32}
+                style={{ marginRight: 5 }}
+              /> */}
+              <p style={{ fontSize: 16, color: "black" }}>
+                Highly skilled at progressive enhancement, design systems & UI
+                Engineering.
+              </p>
+            </Col>
+            <Col md={{ size: 3, offset: 1 }}>
+              <p style={{ fontSize: 16, color: "black" }}>
+                Proven experience building full-stack products across several
+                industries.
+              </p>
+            </Col>
+          </Row>
         </div>
       </div>
     </section>
